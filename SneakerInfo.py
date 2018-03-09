1196a4bfc334262970af2327abb24103ebdb40df
Marcus Cameron <marcus17c@gmail.com>
2018-03-09 12:31:29 -0500

    Update

--- SneakerInfo.py
+++ SneakerInfo.py
@@ -5,17 +5,15 @@ import json
 class NikeShoe:
 
 	def __init__(self, StyleCode):
-		self.URL = 'https://store.nike.com/ca/en_gb/pw/n/1j7?sl=' + str(StyleCode)
-		self.lxml = BeautifulSoup(requests.get(self.URL).content, "lxml")
-
-
-
+		self.URL = 'https://store.nike.com/us/en_us/pw/n/1j7?sl=' + str(StyleCode)
+		self.r = requests.get(self.URL).content
+		self.lxml = BeautifulSoup(self.r, "lxml")
+		self.j = self.lxml.find("script", {"id": "product-data"}).string
 
 	def GetStock(self):
 
 		self.sizesinStock = []
-		self.j = self.lxml.find("script", {"id": "product-data"}).string
-		self.j = json.loads(self.j)
+
 		self.sizesBox = self.j['skuContainer']['productSkus']
 		for size in self.sizesBox:
 			if size['inStock'] == True:
@@ -40,10 +38,13 @@ class NikeShoe:
 
 
 	def GetName(self):
-		return self.lxml.find(class_="exp-product-title").string
+		self.name = self.lxml.find(class_="productTitlee")  # .string
+		return self.name
 
 
 	def GetColorWay(self):
 		return self.lxml.find(class_="colorText").string
 
-s = NikeShoe("AQ0067-101")
+s = NikeShoe("302370-021")
+print(s.GetName())
+
